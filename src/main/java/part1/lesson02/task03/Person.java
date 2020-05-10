ca3feac64b6109933fc200f2a2b2e03da61dd4e9
AlekseyAndrianov <43870626+AlekseyAndrianov@users.noreply.github.com>
2020-05-10 15:31:30 +0300

    lesson02/task03: change comparing Persons

--- src/main/java/part1/lesson02/task03/Person.java
+++ src/main/java/part1/lesson02/task03/Person.java
@@ -18,21 +18,12 @@ public class Person {
     private Sex sex;
     private String name;
 
-    public void setAge(int age) {
-        if (age < 0 || age > 100)
-            throw new IllegalArgumentException("Age must be from 0 to 100");
-        this.age = age;
-    }
-
     public boolean equals(final Object o) {
         if (o == this) return true;
         if (!(o instanceof Person)) return false;
         final Person other = (Person) o;
         if (!other.canEqual((Object) this)) return false;
         if (this.getAge() != other.getAge()) return false;
-        final Object this$sex = this.getSex();
-        final Object other$sex = other.getSex();
-        if (this$sex == null ? other$sex != null : !this$sex.equals(other$sex)) return false;
         final Object this$name = this.getName();
         final Object other$name = other.getName();
         if (this$name == null ? other$name != null : !this$name.equals(other$name)) return false;
@@ -47,8 +38,6 @@ public class Person {
         final int PRIME = 59;
         int result = 1;
         result = result * PRIME + this.getAge();
-        final Object $sex = this.getSex();
-        result = result * PRIME + ($sex == null ? 43 : $sex.hashCode());
         final Object $name = this.getName();
         result = result * PRIME + ($name == null ? 43 : $name.hashCode());
         return result;
