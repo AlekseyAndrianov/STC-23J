89a49abba225da5278286393af0a4926884233b2
AlekseyAndrianov <43870626+AlekseyAndrianov@users.noreply.github.com>
2020-05-17 16:00:48 +0300

    lesson08/task01: Completed

--- src/main/java/part1/lesson06/task01/service/FactorialService.java
+++
@@ -1,8 +0,0 @@
-package part1.lesson06.task01.service;
-
-import java.math.BigInteger;
-import java.util.Map;
-
-public interface FactorialService {
-    Map<Integer, BigInteger> findFactorials(Integer[] numbers);
-}
---
+++ src/main/java/part1/lesson08/task01/Main.java
@@ -0,0 +1,36 @@
+package part1.lesson08.task01;
+
+
+import part1.lesson08.task01.entities.SomeObject;
+import part1.lesson08.task01.service.ObjectSerializer;
+
+/**
+ * Задание 1. Необходимо разработать класс, реализующий следующие методы:
+ * <p>
+ * void serialize (Object object, String file);
+ * Object deSerialize(String file);
+ * Методы выполняют сериализацию объекта Object в файл file и десериализацию объекта из этого файла.
+ * Обязательна сериализация и десериализация "плоских" объектов (все поля объекта - примитивы, или String).
+ */
+public class Main {
+
+    private static final String file = "C:\\Users\\Admin\\Desktop\\destination.txt";
+
+    public static void main(String[] args) {
+
+        SomeObject someObject = SomeObject.builder()
+                .ind(1000L)
+                .name("Example")
+                .size(10)
+                .weight(25.0)
+                .build();
+
+        ObjectSerializer objectSerializer = new ObjectSerializer();
+        objectSerializer.serialize(someObject, file);
+        System.out.println("someObject: " + someObject);
+
+        SomeObject deSerializedObject = (SomeObject) objectSerializer.deSerialize(file);
+        System.out.println("deSerializedObject: " + deSerializedObject);
+    }
+
+}
