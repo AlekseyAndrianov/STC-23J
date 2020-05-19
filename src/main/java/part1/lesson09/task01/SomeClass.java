b314769262ddd8f5e1d6d5332ab2d81e35befa14
AlekseyAndrianov <43870626+AlekseyAndrianov@users.noreply.github.com>
2020-05-19 12:28:59 +0300

    lesson10/task01: init

--- src/main/java/part1/lesson09/task01/SomeClass.java
+++
@@ -1,8 +0,0 @@
-package part1.lesson09.task01;
-
-public class SomeClass implements Worker {
-    @Override
-    public void doWork() {
-
-    }
-}
--- src/main/java/part1/lesson09/task01/Worker.java
+++
@@ -1,7 +0,0 @@
-package part1.lesson09.task01;
-
-public interface Worker {
-
-    void doWork();
-
-}
